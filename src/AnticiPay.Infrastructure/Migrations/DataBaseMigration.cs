94ad825bfdadc31e36f474edbe2aa28eebaa3969
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-24 16:16:46 -0300

    feat: add migração automática no banco de dados ao rodar a aplicação

---
+++ src/AnticiPay.Infrastructure/Migrations/DataBaseMigration.cs
@@ -0,0 +1,14 @@
+﻿using AnticiPay.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AnticiPay.Infrastructure.Migrations;
+public class DataBaseMigration
+{
+    public async static Task MigrateDatabase(IServiceProvider serviceProvider)
+    {
+        var dbContext = serviceProvider.GetRequiredService<AnticiPayDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+    }
+}
