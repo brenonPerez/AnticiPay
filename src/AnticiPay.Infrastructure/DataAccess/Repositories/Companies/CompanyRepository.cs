5756a941e3ff230b06651741cab7a56289f2174a
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-24 16:23:47 -0300

    feat: adicionar repositório de empresas e sua configuração de injeção de dependência

---
+++ src/AnticiPay.Infrastructure/DataAccess/Repositories/Companies/CompanyRepository.cs
@@ -0,0 +1,17 @@
+﻿using AnticiPay.Domain.Entities;
+using AnticiPay.Domain.Repositories.Companies;
+
+namespace AnticiPay.Infrastructure.DataAccess.Repositories.Companies;
+internal class CompanyRepository : ICompanyRepository
+{
+    private readonly AnticiPayDbContext _dbContext;
+    public CompanyRepository(AnticiPayDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task Add(Company company)
+    {
+        await _dbContext.Companies.AddAsync(company);
+        await _dbContext.SaveChangesAsync();
+    }
+}
--- src/AnticiPay.Infrastructure/DependencyInjectionExtension.cs
+++ src/AnticiPay.Infrastructure/DependencyInjectionExtension.cs
@@ -1,4 +1,6 @@
-﻿using AnticiPay.Infrastructure.DataAccess;
+﻿using AnticiPay.Domain.Repositories.Companies;
+using AnticiPay.Infrastructure.DataAccess;
+using AnticiPay.Infrastructure.DataAccess.Repositories.Companies;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +9,17 @@ namespace AnticiPay.Infrastructure;
 public static class DependencyInjectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext(configuration);
+        services.AddRepositories();
+    }
+
+    private static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ICompanyRepository, CompanyRepository>();
+    }
+
+    private static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Connection");
 
