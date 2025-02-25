5fa301f722cb67e3230740553c352f17f1336ddd
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-25 18:47:37 -0300

    feat: adicionar repositório de atualização de notas fiscais

---
+++ src/AnticiPay.Domain/Repositories/Invoices/IInvoiceUpdateOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿using AnticiPay.Domain.Entities;
+
+namespace AnticiPay.Domain.Repositories.Invoices;
+public interface IInvoiceUpdateOnlyRepository
+{
+    Task<Invoice?> GetInvoiceNotInCart(long invoiceId);
+
+    void Update(Invoice invoice);
+}
--- src/AnticiPay.Infrastructure/DataAccess/Repositories/Invoices/InvoiceRespository.cs
+++ src/AnticiPay.Infrastructure/DataAccess/Repositories/Invoices/InvoiceRespository.cs
@@ -3,7 +3,7 @@ using AnticiPay.Domain.Repositories.Invoices;
 using Microsoft.EntityFrameworkCore;
 
 namespace AnticiPay.Infrastructure.DataAccess.Repositories.Invoices;
-internal class InvoiceRespository : IInvoiceWriteOnlyRepository, IInvoiceReadOnlyRepository
+internal class InvoiceRespository : IInvoiceWriteOnlyRepository, IInvoiceReadOnlyRepository, IInvoiceUpdateOnlyRepository
 {
     private readonly AnticiPayDbContext _dbContext;
     public InvoiceRespository(AnticiPayDbContext dbContext)
@@ -19,5 +19,17 @@ internal class InvoiceRespository : IInvoiceWriteOnlyRepository, IInvoiceReadOnl
     public async Task<bool> ExistInvoiceWithNumber(string number)
     {
         return await _dbContext.Invoices.AnyAsync(i => i.Number.Equals(number));
+    }
+
+    public async Task<Invoice?> GetInvoiceNotInCart(long invoiceId)
+    {
+        return await _dbContext.Invoices
+            .Where(i => i.Id == invoiceId && i.CartId == null)
+            .FirstOrDefaultAsync();
+    }
+
+    public void Update(Invoice invoice)
+    {
+        _dbContext.Invoices.Update(invoice);
     }
 }
