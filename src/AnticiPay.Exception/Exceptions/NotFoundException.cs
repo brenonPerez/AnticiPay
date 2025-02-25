6692af7e5243027c5f285f687517863142b0c099
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-25 17:58:34 -0300

    feat: add exceção personalizada NotFoundException

---
+++ src/AnticiPay.Exception/Exceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using AnticiPay.Exception.Exceptions.Base;
+using System.Net;
+
+namespace AnticiPay.Exception.Exceptions;
+public class NotFoundException : AnticiPayException
+{
+    public NotFoundException(string message) : base(message)
+    {
+    }
+
+    public override int StatusCode => (int)HttpStatusCode.NotFound;
+
+    public override List<string> GetErrors()
+    {
+        return [Message];
+    }
+}
