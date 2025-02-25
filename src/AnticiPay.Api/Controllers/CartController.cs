482218c32483c3a1dc752f0425662c4f4f66d2dd
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-25 19:05:09 -0300

    feat: add endpoint para remover notas fiscais incluída no carrinho

--- src/AnticiPay.Api/Controllers/CartController.cs
+++ src/AnticiPay.Api/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using AnticiPay.Application.UseCases.Carts.AddInvoice;
+using AnticiPay.Application.UseCases.Carts.RemoveInvoice;
 using AnticiPay.Communication.Requests;
 using AnticiPay.Communication.Responses;
 using Microsoft.AspNetCore.Authorization;
@@ -20,5 +21,17 @@ public class CartController : ControllerBase
         await useCase.Execute(request);
 
         return Created(string.Empty, string.Empty);
+    }
+
+    [HttpDelete("remove-invoice")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> RemoveInvoiceFromCart(
+    [FromServices] IRemoveInvoiceFromCartUseCase useCase,
+    [FromBody] RequestInvoiceCartJson request)
+    {
+        await useCase.Execute(request);
+
+        return Ok(string.Empty);
     }
 }
---
+++ src/AnticiPay.Application/UseCases/Carts/RemoveInvoice/IRemoveInvoiceFromCartUseCase.cs
@@ -0,0 +1,7 @@
+﻿using AnticiPay.Communication.Requests;
+
+namespace AnticiPay.Application.UseCases.Carts.RemoveInvoice;
+public interface IRemoveInvoiceFromCartUseCase
+{
+    Task Execute(RequestInvoiceCartJson request);
+}
