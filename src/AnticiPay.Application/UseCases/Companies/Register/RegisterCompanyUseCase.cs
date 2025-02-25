c8156a814ddefdf1ce52e553fc894daa7e1faa9a
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-24 21:30:18 -0300

    feat: implementar validações no caso de uso ao registrar empresa

--- src/AnticiPay.Application/UseCases/Companies/Register/RegisterCompanyUseCase.cs
+++ src/AnticiPay.Application/UseCases/Companies/Register/RegisterCompanyUseCase.cs
@@ -1,38 +1,54 @@
 ﻿using AnticiPay.Communication.Requests;
 using AnticiPay.Communication.Responses;
 using AnticiPay.Domain.Entities;
-using AnticiPay.Domain.Enums;
 using AnticiPay.Domain.Repositories.Companies;
+using AnticiPay.Domain.Utils;
 using AnticiPay.Exception.ExceptionsBase;
 using AutoMapper;
+using FluentValidation.Results;
 
 namespace AnticiPay.Application.UseCases.Companies.Register;
 public class RegisterCompanyUseCase : IRegisterCompanyUseCase
 {
-    private readonly ICompanyWriteOnlyRepository _companyRepository;
+    private readonly ICompanyWriteOnlyRepository _companyWriteOnlyRepository;
+    private readonly ICompanyReadOnlyRepository _companyReadOnlyRepository;
     private readonly IMapper _mapper;
-    public RegisterCompanyUseCase(ICompanyWriteOnlyRepository companyRepository,
+    public RegisterCompanyUseCase(ICompanyWriteOnlyRepository companyWriteOnlyRepository,
+        ICompanyReadOnlyRepository companyReadOnlyRepository,
         IMapper mapper)
     {
-        _companyRepository = companyRepository;
+        _companyWriteOnlyRepository = companyWriteOnlyRepository;
+        _companyReadOnlyRepository = companyReadOnlyRepository;
         _mapper = mapper;
     }
 
     public async Task<ResponseRegisteredCompanyJson> Execute(RequestRegisterCompanyJson request)
     {
-        Validate(request);
+        await Validate(request);
 
         var company = _mapper.Map<Company>(request);
 
-        await _companyRepository.Add(company);
+        await _companyWriteOnlyRepository.Add(company);
 
         return _mapper.Map<ResponseRegisteredCompanyJson>(company);
     }
 
-    public static void Validate(RequestRegisterCompanyJson request)
+    public async Task Validate(RequestRegisterCompanyJson request)
     {
         var result = new RegisterCompanyValidator().Validate(request);
 
+        var emailAlreadyExists = await _companyReadOnlyRepository.ExistActiveCompanyWithEmail(request.Email);
+        if (emailAlreadyExists)
+        {
+            result.Errors.Add(new ValidationFailure(string.Empty, "Email already exists"));
+        }
+
+        var cnpjAlreadyExists = await _companyReadOnlyRepository.ExistActiveCompanyWhithCnpj(CnpjUtils.Normalize(request.Cnpj));
+        if (cnpjAlreadyExists)
+        {
+            result.Errors.Add(new ValidationFailure(string.Empty, "Cnpj already exists"));
+        }
+        
         if (result.IsValid is false)
         {
             var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
