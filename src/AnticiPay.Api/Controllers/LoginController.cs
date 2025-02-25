90e52f12a7abbfd8031e5a3f1ac5fc7c73d208a5
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-24 23:26:31 -0300

    feat: adicionar LoginController para autenticação de das empresas

---
+++ src/AnticiPay.Api/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using AnticiPay.Application.UseCases.Login.DoLogin;
+using AnticiPay.Communication.Requests;
+using AnticiPay.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnticiPay.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredCompanyJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login(
+        [FromServices] IDoLoginUseCase useCase,
+        [FromBody] RequestLoginJson request)
+    {
+        var response = await useCase.Execute(request);
+
+        return Ok(response);
+    }
+}
---
+++ src/AnticiPay.Application/UseCases/Login/DoLogin/IDoLoginUseCase.cs
@@ -0,0 +1,8 @@
+﻿using AnticiPay.Communication.Requests;
+using AnticiPay.Communication.Responses;
+
+namespace AnticiPay.Application.UseCases.Login.DoLogin;
+public interface IDoLoginUseCase
+{
+    Task<ResponseRegisteredCompanyJson> Execute(RequestLoginJson request);
+}
