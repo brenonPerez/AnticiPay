327ff43c6dffb8e6dfff95148c65ae90493c2097
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-25 16:25:56 -0300

    feat: add migração para entidade carrinho de antecipação

--- src/AnticiPay.Infrastructure/DataAccess/AnticiPayDbContext.cs
+++ src/AnticiPay.Infrastructure/DataAccess/AnticiPayDbContext.cs
@@ -10,4 +10,22 @@ internal class AnticiPayDbContext : DbContext
 
     public DbSet<Company> Companies { get; set; }
     public DbSet<Invoice> Invoices { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Cart>()
+            .HasMany(c => c.Invoices)
+            .WithOne(i => i.Cart)
+            .HasForeignKey(i => i.CartId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Invoice>()
+            .HasOne(i => i.Cart)
+            .WithMany(c => c.Invoices)
+            .HasForeignKey(i => i.CartId)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
 }
---
+++ src/AnticiPay.Infrastructure/Migrations/20250225192121_AddCartMigration.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnticiPay.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCartMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CartId",
+                table: "Invoices",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CompanyId = table.Column<long>(type: "bigint", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Carts_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_CartId",
+                table: "Invoices",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_CompanyId",
+                table: "Carts",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Carts_CartId",
+                table: "Invoices",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Carts_CartId",
+                table: "Invoices");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_CartId",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "Invoices");
+        }
+    }
+}
