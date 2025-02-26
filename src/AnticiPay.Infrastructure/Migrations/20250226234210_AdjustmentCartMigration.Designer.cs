d957e066791947fac7be8b15fda5cc727d7d790f
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-26 20:55:31 -0300

    feat: add migration para ajustar tabelas de carrinho e faturas para o checkout

--- src/AnticiPay.Infrastructure/Migrations/20250226234210_AdjustmentCartMigration.Designer.cs
+++ src/AnticiPay.Infrastructure/Migrations/20250226234210_AdjustmentCartMigration.Designer.cs
@@ -4,6 +4,7 @@ using AnticiPay.Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AnticiPay.Infrastructure.Migrations
 {
     [DbContext(typeof(AnticiPayDbContext))]
-    partial class AnticiPayDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250226234210_AdjustmentCartMigration")]
+    partial class AdjustmentCartMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AnticiPay.Infrastructure/Migrations/20250226234210_AdjustmentCartMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnticiPay.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustmentCartMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "NetValueAtCheckout",
+                table: "Invoices",
+                type: "decimal(65,30)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckoutDate",
+                table: "Carts",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TaxRateAtCheckout",
+                table: "Carts",
+                type: "decimal(65,30)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NetValueAtCheckout",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "CheckoutDate",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "TaxRateAtCheckout",
+                table: "Carts");
+        }
+    }
+}
